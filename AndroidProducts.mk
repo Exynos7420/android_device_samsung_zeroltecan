fa2ef073769f755bbf75ac5d3d56004ed5316b11
enesuzun2002 <enesuzun200227@gmail.com>
2019-12-09 17:57:07 +0000

    zero: give pixel experience another chance

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,4 +1,5 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/full_zeroltecan.mk \
     $(LOCAL_DIR)/lineage_zeroltecan.mk \
-    $(LOCAL_DIR)/viper_zeroltecan.mk
+    $(LOCAL_DIR)/viper_zeroltecan.mk \
+    $(LOCAL_DIR)/aosp_zeroltecan.mk
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -16,3 +16,4 @@
 
 add_lunch_combo lineage_zeroltecan-userdebug
 add_lunch_combo viper_zeroltecan-userdebug
+add_lunch_combo aosp_zeroltecan-userdebug
